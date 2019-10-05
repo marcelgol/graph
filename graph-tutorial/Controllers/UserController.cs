6440d16f4ec3aa91cfd7eac171fbc9e672e4e52f
Marcel_Goldhardt <Marcel_Goldhardt@W10GDBMVG2>
2019-10-05 16:13:00 -0300

    Initial Commit. Added MS Graph sample with changes

---
+++ graph-tutorial/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using graph_tutorial.Helpers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: User
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+            var users = await GraphHelper.GetUsersAsync();
+
+            return View(users);
+        }
+    }
+}
